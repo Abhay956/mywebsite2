3b61409894865cf4ba1d0d7d9cca6f3e0397bea9
Abhay956 <132220412+Abhay956@users.noreply.github.com>
2023-09-09 17:31:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 body,h1 {font-family: "Raleway", sans-serif}
 body, html {height: 100%}
 .bgimg {
-  background-image: url('https://wallpapers.com/images/hd/coniferous-forest-lake-bub7kqwrdxapq1t8.webp');
+  background-image: url('https://wallpapers.com/images/hd/lush-green-forest-neblzag5t76yolal.webp');
   min-height: 100%;
   background-position: center;
   background-size: cover;
